2c1588526a6d68f8e45ff7db9325bea0ddb63cf2
Kris Hopper <hopperdavis@gmail.com>
2021-07-23 08:40:36 -0700

    fail first three specs for basic functionality

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,13 +1,37 @@
 require('rspec')
-require('title_case')
+require('word')
 require('pry')
 
-describe('#title_case') do
-  it("capitalizes the first letter of a word") do
-    expect(title_case("beowulf")).to(eq("Beowulf"))
+describe '#Word' do
+
+  before(:each) do
+    Word.clear()
   end
 
-  it("capitalizes the first letter of all words in a multiple word title") do
-    expect(title_case("the color purple")).to(eq("The Color Purple"))
+  describe('.all') do
+    it("returns an empty array when there are no words") do
+      expect(Word.all).to(eq([]))
+    end
   end
+
+  describe('#save') do
+    it("saves a word") do
+      word = Word.new("Unicorn")
+      word.save()
+      word2 = Word.new("Pegasus")
+      word2.save()
+      expect(Word.all).to(eq([word, word2]))
+    end
+  end
+
+  describe('#==') do
+    it("is the same word if it has the same attributes as another word") do
+      word = Word.new("Unicorn")
+      word2 = Word.new("Unicorn")
+      expect(word).to(eq(word2))
+    end
+  end
+
+  
+
 end
