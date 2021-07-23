f1e4e3c6599ca1ed4e4a10c38d68f505cbddd170
Kris Hopper <hopperdavis@gmail.com>
2021-07-23 11:12:10 -0700

    add new views and functionality to app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
+require('./lib/definition')
 also_reload('lib/**/*.rb')
 require 'pry'
+
+get('/') do
+  @words = Word.all
+  erb(:word_definer)
+end
+
+get('/word/new') do
+  erb(:new_word)
+end
